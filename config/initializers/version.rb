4e0fbe78d3ace3d2d958b523cf59fe54ebcfc434
John Romkey <58883+romkey@users.noreply.github.com>
2025-12-15 13:38:46 -0800

    v0.1.13: Add footer with version and GitHub link
    - VERSION file stores current version
    - Footer on all pages shows version and links to GitHub
    - Version links to specific release tag

---
+++ config/initializers/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Geowiki
+  VERSION = File.read(Rails.root.join("VERSION")).strip.freeze
+  GITHUB_URL = "https://github.com/romkey/pdxhackerspace-geowiki"
+end
+
