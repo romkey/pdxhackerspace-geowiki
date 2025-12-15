a4682f0b69d532d2d05d77463272b7b4a605ec58
John Romkey <58883+romkey@users.noreply.github.com>
2025-12-15 15:05:04 -0800

    v0.3.1: Improve resource type selection UI
    - Replace confusing 'Internal' toggle with clear radio buttons
    - Add icons and descriptions for On-site vs Off-site
    - Makes it clear what each option means

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2024_12_14_000017) do
+ActiveRecord::Schema[8.1].define(version: 2024_12_14_000018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -116,6 +116,7 @@ ActiveRecord::Schema[8.1].define(version: 2024_12_14_000017) do
   create_table "resources", force: :cascade do |t|
     t.boolean "admin_only", default: false, null: false
     t.datetime "created_at", null: false
+    t.text "description"
     t.string "icon"
     t.boolean "internal", default: true, null: false
     t.decimal "latitude", precision: 10, scale: 7
